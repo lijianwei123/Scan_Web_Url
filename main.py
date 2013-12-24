e3e20406ab4fb396a6f46e6255b8d74e7c1c155b
lijianwei123 <lijianwei_123@126.com>
2013-12-25 02:12:09 +0800

    实现获取鼠标选择文字 借助clipboard实现，方法有些丑陋了哦

--- main.py
+++ main.py
@@ -13,15 +13,63 @@ import win32con
 
 import SimulationKey
 
+import SendKeys # 安装 http://pythonxy.googlecode.com/files/SendKeys-0.3_py27.exe
+
+#sendkeys 示例  http://msdn.microsoft.com/zh-cn/library/system.windows.forms.sendkeys.send%28v=vs.110%29.aspx
+
+import urllib
+import urllib2
+
 
 def throw_phone():
-    #模拟按键
-    SimulationKey.press('ctrl', 'alt', 'esc')
+    org_clip_data = clipboard.getText()
     
-    print clipboard.getText()
+    #模拟按键
+    #SimulationKey.press('left_menu')
+    SendKeys.SendKeys("^c") #复制文字
+    
+    #把文字提交到服务端
+    postText(clipboard.getText())
+    
+    #还原
+    clipboard.setText(org_clip_data)
+    
+def postText(text):
+    if not text.strip():
+        return False
+    else:
+        form_data = {"text": convertUtf8(text)};
+        return post(**form_data)
+        
+def post(**kargs):
+    url = "http://localhost/record.php"
+    req = urllib2.Request(url, urllib.urlencode(kargs))
+    resp = urllib2.urlopen(req)
+    return resp.read()
+
+def convertUtf8(s):
+    encoding = mb_detect_encoding(s)
+    if isinstance(s, unicode):
+        return s.encode("utf-8")
+    else:
+        return s.decode(encoding).encode("utf-8")
+
+
+def mb_detect_encoding(text, encoding_list=['gb2312', 'utf-8']):
+    '''Return first matched encoding in encoding_list, otherwise return None.
+    See [url]http://docs.python.org/2/howto/unicode.html#the-unicode-type[/url] for more info.
+    See [url]http://docs.python.org/2/library/codecs.html#standard-encodings[/url] for encodings.'''
+    for best_enc in encoding_list:
+        try:
+            unicode(text, best_enc)
+        except:
+            best_enc = None
+        else:
+            break
+    return best_enc
 
 hot = pyhk.pyhk()
 
-hot.addHotkey(['Alt', '3'], throw_phone);
+hot.addHotkey(['3'], throw_phone);
 
 hot.start()
---
+++ test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+str = "lijianwei"
+list = list(str)
+print list.pop()
